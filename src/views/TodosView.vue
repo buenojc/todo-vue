06af2f02bf5c1a0bbc43c942458733392eb73dfb
Julio Cesar Bueno <buenojc@outlook.com>
2023-03-20 17:27:02 -0300

    add: todo item e mensagem quando não há tarefas

--- src/views/TodosView.vue
+++ src/views/TodosView.vue
@@ -2,6 +2,8 @@
 import { ref } from 'vue';
 import { uid } from 'uid';
 import TodoCreator from '../components/TodoCreator.vue';
+import TodoItem from '../components/TodoItem.vue';
+import { Icon } from '@iconify/vue';
 const todoList = ref([])
 
 const createTodo = (todo) => {
@@ -23,6 +25,27 @@ const createTodo = (todo) => {
         Essa função recebe o dado emitido
       -->
     <TodoCreator @create-todo="createTodo" />
+
+    <ul class="todo-list" v-if="todoList.length > 0">
+      <!-- O v-for é uma forma de se aplicar um for no vue. 
+        Neste caso o componente TodoItem vai ser renderizado uma vez
+        para cada item no todoList
+      -->
+
+      <!-- 
+        :todo é a forma de passar parâmetros para o componente. 
+        : + nome do parâmetro = valor do parâmetro
+
+        Também é necessário preparar o componente para aceitar parâmetros
+       -->
+      
+      <TodoItem v-for="todo in todoList" :todo="todo" />
+    </ul>
+
+    <p v-else class="todos-msg">
+      <Icon icon="noto-v1:sad-but-relieved-face"  width="22" />
+      <span>Você não criou nenhuma tarefa. Crie uma!</span>
+    </p>
   </main>
 </template>
 
@@ -38,6 +61,22 @@ main {
   h1 {
     margin-bottom: 16px;
     text-align: center;
+  }
+
+  .todo-list{
+    display: flex;
+    flex-direction: column;
+    list-style: none;
+    margin-top: 24px;
+    gap: 20px;
+  }
+
+  .todos-msg{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 8px;
+    margin-top: 24px;
   }
 }
 </style>
