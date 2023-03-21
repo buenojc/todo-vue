58dcf6f33736f0e7081a7a64225d90038cf31a57
Julio Cesar Bueno <buenojc@outlook.com>
2023-03-21 19:54:47 -0300

    add: atualização e delete do todo

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -3,29 +3,36 @@
     import { Icon } from '@iconify/vue'
 
     // Forma de definir os props. É possível definir o tipo, se é obrigatório e até valor default
+    // É necessário definir os props para que eles sejam usáveis no componente
     const props = defineProps({
         todo: {
             type: Object,
             required: true,
+        },
+        index: {
+            type: Number,
+            required:true
         }
-    })
+    });
+
+    defineEmits(['toggle-complete', 'edit-todo', 'update-todo', 'delete-todo'])
 </script>
 
 <template>
     <li>
-        <input type="checkbox" :checked="todo.isCompleted"/>
+        <input type="checkbox" :checked="todo.isCompleted" @input="$emit('toggle-complete', index)"/>
         <div class="todo">
             <!-- Conjunto if else no vue -->
-            <input v-if="todo.isEditing" type="text" :value="todo.todo" />
-            <span v-else>
+            <input v-if="todo.isEditing" type="text" :value="todo.todo" @input="$emit('update-todo', $event.target.value, index)" />
+            <span v-else :class="{ 'completed-todo': todo.isCompleted}">
                 {{ todo.todo }}
             </span>
             
         </div>
         <div class="todo-actions">
-            <Icon v-if="todo.isEditing" icon="ph:check-circle" class="icon" color="#41b080" width="22" />
-            <Icon v-else icon="ph:pencil-fill" class="icon" color="#41b080" width="22" />
-            <Icon icon="ph:trash" class="icon" color="#f95e5e" width="22" />
+            <Icon v-if="todo.isEditing" icon="ph:check-circle" @click="$emit('edit-todo', index)" class="icon" color="#41b080" width="22" />
+            <Icon v-else icon="ph:pencil-fill" @click="$emit('edit-todo', index)" class="icon" color="#41b080" width="22" />
+            <Icon icon="ph:trash" class="icon" color="#f95e5e" width="22" @click="$emit('delete-todo', todo.id)" />
         </div>
     </li>
 </template>
@@ -64,11 +71,16 @@
         .todo{
             flex: 1;
 
+            .completed-todo{
+                text-decoration: line-through;
+            }
+
             input[type="text"]{
                 width: 100%;
                 padding: 2px 6px;
                 border: 2px solid #41b080;
             }
+
         }
 
         .todo-actions{
--- src/views/TodosView.vue
+++ src/views/TodosView.vue
@@ -15,35 +15,53 @@ const createTodo = (todo) => {
     isEditing: null
   })
 }
+
+const toggleTodoComplete = (todoPosition) => {
+  todoList.value[todoPosition].isCompleted = !todoList.value[todoPosition].isCompleted;
+}
+
+const toggleEdit = (todoPosition) => {
+  todoList.value[todoPosition].isEditing = !todoList.value[todoPosition].isEditing;
+}
+
+const updateTodo = (todoValue, todoPosition) => {
+  todoList.value[todoPosition].todo = todoValue
+}
+
+const deleteTodo = (todoId) => {
+  todoList.value = todoList.value.filter(todo => todo.id !== todoId)
+}
+
 </script>
 
 <template>
   <main>
     <h1>Create Todo</h1>
     <!-- A partir do momento que é criado um emit, ele fica acessível
-        à outros componentes onde esse componente emissor está usando o @ + nome do emissor
-        Essa função recebe o dado emitido
-      -->
+            à outros componentes onde esse componente emissor está usando o @ + nome do emissor
+            Essa função recebe o dado emitido
+          -->
     <TodoCreator @create-todo="createTodo" />
 
     <ul class="todo-list" v-if="todoList.length > 0">
       <!-- O v-for é uma forma de se aplicar um for no vue. 
-        Neste caso o componente TodoItem vai ser renderizado uma vez
-        para cada item no todoList
-      -->
+            Neste caso o componente TodoItem vai ser renderizado uma vez
+            para cada item no todoList
+          -->
 
       <!-- 
-        :todo é a forma de passar parâmetros para o componente. 
-        : + nome do parâmetro = valor do parâmetro
+            :todo é a forma de passar parâmetros para o componente. 
+            : + nome do parâmetro = valor do parâmetro
 
-        Também é necessário preparar o componente para aceitar parâmetros
-       -->
-      
-      <TodoItem v-for="todo in todoList" :todo="todo" />
+            Também é necessário preparar o componente para aceitar parâmetros
+           -->
+
+      <TodoItem v-for="(todo, index) in todoList" :todo="todo" :index="index" @toggle-complete="toggleTodoComplete"
+        @edit-todo="toggleEdit" @update-todo="updateTodo" @delete-todo="deleteTodo"/>
     </ul>
 
     <p v-else class="todos-msg">
-      <Icon icon="noto-v1:sad-but-relieved-face"  width="22" />
+      <Icon icon="noto-v1:sad-but-relieved-face" width="22" />
       <span>Você não criou nenhuma tarefa. Crie uma!</span>
     </p>
   </main>
@@ -63,7 +81,7 @@ main {
     text-align: center;
   }
 
-  .todo-list{
+  .todo-list {
     display: flex;
     flex-direction: column;
     list-style: none;
@@ -71,7 +89,7 @@ main {
     gap: 20px;
   }
 
-  .todos-msg{
+  .todos-msg {
     display: flex;
     align-items: center;
     justify-content: center;
